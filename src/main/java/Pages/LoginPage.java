b028f945777d7a497ee51a4ffc240a5ced15823c
mohamed <mohamedelroo@gmail.com>
2022-09-25 18:19:10 +0200

    Adding specialties

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -3,6 +3,10 @@ package Pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginPage extends PageBase {
 
@@ -12,8 +16,26 @@ public class LoginPage extends PageBase {
     @FindBy (css = "a[routerlink='/Registration']")
     WebElement registerNow;
 
+    @FindBy (id = "loginEmail")
+    WebElement Email;
+
+    @FindBy (id = "loginPassword")
+    WebElement Pass;
+
+    @FindBy (xpath = "/html/body/app-root/div/app-auth/div/div[2]/app-login/div/form/button")
+    WebElement loginButton;
+
     public void openRegistrationPage (){
         clickOnElement(registerNow);
+    }
+
+    public void completeLoginInput (String mail,String password){
+        insertTextIntoElement(Email,mail);
+        insertTextIntoElement(Pass,password);
+    }
+    public void login(){
+      new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.elementToBeClickable(loginButton));
+      clickOnElement(loginButton);
     }
 
 
